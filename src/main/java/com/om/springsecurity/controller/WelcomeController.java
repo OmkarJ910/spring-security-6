565f9a87154e12b1c86dcb84fb60ce5fb9d5712a
Omkar <meetomhere2001@gmail.com>
2025-10-03 08:12:24 +0530

    Initial Commit

---
+++ src/main/java/com/om/springsecurity/controller/WelcomeController.java
@@ -0,0 +1,21 @@
+package com.om.springsecurity.controller;
+
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.security.web.csrf.CsrfToken;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WelcomeController {
+
+    @GetMapping
+    public String Welcome(){
+        return "welcome Omkar";
+    }
+
+    @GetMapping("/csrf")
+    public CsrfToken getToken(HttpServletRequest request){
+        return (CsrfToken) request.getAttribute("_csrf");
+    }
+}
